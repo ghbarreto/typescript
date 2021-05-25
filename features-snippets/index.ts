d785a5cc49a7a1cfedb57f0761a59944c9aca3e2
ivlen <gh.barreto@hotmail.com>
2021-05-25 08:38:37 -0300

    a

---
+++ features-snippets/index.ts
@@ -0,0 +1,13 @@
+const today = new Date();
+today.getMonth();
+// today.gaokgsaksgasg(); <-- throws an err because TS knows the properties
+
+const person = {
+  age: 26
+}
+
+class Color{
+
+}
+
+const red = new Color()
