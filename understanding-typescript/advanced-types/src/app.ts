e44bfc4b7614ac5b77febfe83f8c753ac91e24ba
Gabriel Barreto <gh.barreto@hotmail.com>
2021-09-06 02:10:56 -0300

    a

--- understanding-typescript/advanced-types/src/app.ts
+++ understanding-typescript/advanced-types/src/app.ts
@@ -22,6 +22,12 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+// function overload
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
+function add(a: number, b: string): string;
+
 // type guards
 function add(a: Combinable, b: Combinable) {
   if (typeof a === 'string' || typeof b === 'string') {
@@ -29,3 +35,108 @@ function add(a: Combinable, b: Combinable) {
   }
   return a + b;
 }
+const result = add('Gabriel', ' Barreto');
+result.split(' ');
+
+const fetchUserData = {
+  id: 'u1',
+  name: 'Gabriel',
+  job: { title: 'CEO', descriptiong: 'company' },
+};
+
+console.log(fetchUserData?.job.title); // returns CEO
+
+const userInput = null;
+const storedData = userInput ?? 'DEFAULT';
+// - ?? stands for either undefined or null.
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log('Name: ' + emp.name);
+
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
+  }
+
+  if ('startDate' in emp) {
+    console.log('Start date: ' + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+
+class Car {
+  drive() {
+    console.log('driving...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('Driving a truck');
+  }
+
+  loadCargo(amount: number) {
+    console.log('Loading cargo...' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+// working with objects and union types.
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+  }
+  console.log('Moving speed: ' + speed);
+}
+
+moveAnimal({ type: 'bird', flyingSpeed: 20 });
+
+// type casting
+const userInputElement = document.getElementById(
+  'user-input'
+)! as HTMLInputElement;
+userInputElement.value = 'hi there';
+
+// index types (creates objects that are more flexible)
+interface ErrorContainer {
+  // define an index type
+  [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email',
+  username: 'Not a valid username',
+};
